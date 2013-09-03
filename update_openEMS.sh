d65046f29f2c9685f0ee59a351b4c7e40f94fcf6
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2013-09-03 17:28:20 +0200

    add update shell script
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

---
+++ update_openEMS.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+basedir=$(pwd)
+QMAKE=qmake-qt4
+
+#update all
+echo "updating git submodules... please wait"
+git submodule update
+if [ $? -ne 0 ]; then
+  echo "git submodule update failed!"
+  exit
+fi
+
+#build CSXCAD
+cd CSXCAD
+make clean &> /dev/null
+$QMAKE PREFIX=. CSXCAD.pro
+if [ $? -ne 0 ]; then
+  echo "qmake for CSXCAD failed"
+  cd ..
+  exit
+fi
+
+echo "compiling CSXCAD ... please wait"
+make -j4 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "make for CSXCAD failed"
+  cd ..
+  exit
+fi
+make install > /dev/null
+if [ $? -ne 0 ]; then
+  echo "make install for CSXCAD failed"
+  cd ..
+  exit
+fi
+cd ..
+
+#build QCSXCAD
+cd QCSXCAD
+make clean &> /dev/null
+$QMAKE PREFIX=. CSXCAD_ROOT=$basedir/CSXCAD QCSXCAD.pro
+if [ $? -ne 0 ]; then
+  echo "qmake for QCSXCAD failed"
+  cd ..
+  exit
+fi
+
+echo "compiling QCSXCAD ... please wait"
+make -j4 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "make for QCSXCAD failed"
+  cd ..
+  exit
+fi
+make install > /dev/null
+if [ $? -ne 0 ]; then
+  echo "make install for QCSXCAD failed"
+  cd ..
+  exit
+fi
+cd ..
+
+#build AppCSXCAD
+cd AppCSXCAD
+make clean &> /dev/null
+$QMAKE PREFIX=. CSXCAD_ROOT=$basedir/CSXCAD QCSXCAD_ROOT=$basedir/QCSXCAD AppCSXCAD.pro
+if [ $? -ne 0 ]; then
+  echo "qmake for AppCSXCAD failed"
+  cd ..
+  exit
+fi
+
+echo "compiling AppCSXCAD ... please wait"
+make -j4 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "make for AppCSXCAD failed"
+  cd ..
+  exit
+fi
+cd ..
+
+#build openEMS
+cd openEMS
+make clean &> /dev/null
+$QMAKE PREFIX=. CSXCAD_ROOT=$basedir/CSXCAD openEMS.pro
+if [ $? -ne 0 ]; then
+  echo "qmake for openEMS failed"
+  cd ..
+  exit
+fi
+
+echo "compiling openEMS ... please wait"
+make -j4 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "make for openEMS failed"
+  cd ..
+  exit
+fi
+cd ..
+
+#build nf2ff
+cd openEMS/nf2ff
+make clean &> /dev/null
+$QMAKE
+echo "compiling nf2ff ... please wait"
+make -j4 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "make for nf2ff failed"
+  cd ../..
+  exit
+fi
+cd ../..
+
+echo "openEMS and all modules have been updated successfully..."
+echo ""
+echo "add the required pathes to Octave/Matlab:"
+echo "addpath('$basedir/openEMS/matlab')"
+echo "addpath('$basedir/CSXCAD/matlab')"
+echo ""
+echo "Have fun using openEMS"
