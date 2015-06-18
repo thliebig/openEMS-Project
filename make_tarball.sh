dd5d9b71ed08321b90b91136f6195c7312c1e045
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2015-06-18 20:31:20 +0200

    make tarball script
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

---
+++ make_tarball.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd openEMS
+GITREV=`git describe --tags`
+cd ..
+echo $GITREV
+
+rm -f openEMS-$GITREV.tar
+rm -f openEMS-$GITREV.tar.bz2
+
+git archive --format=tar HEAD -o openEMS-$GITREV.tar
+
+for mod in fparser CSXCAD QCSXCAD AppCSXCAD openEMS CTB hyp2mat; do
+  cd $mod || exit 1
+  tmpfn=`mktemp --suffix=tar`
+  git archive --format=tar --prefix=$mod/ HEAD -o $tmpfn
+  cd ..
+  tar --concatenate --file=openEMS-$GITREV.tar $tmpfn
+  rm -f $tmpfn
+done
+
+bzip2 openEMS-$GITREV.tar
+rm -f openEMS-$GITREV.tar
