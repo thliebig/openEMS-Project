b9e98c2449c8552a5749533daca74d834a173b78
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2015-11-14 14:37:59 +0100

    tarball: add "openEMS/" to prefix
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

--- make_tarball.sh
+++ make_tarball.sh
@@ -7,12 +7,12 @@ cd ..
 rm -f openEMS-$GITREV.tar
 rm -f openEMS-$GITREV.tar.bz2
 
-git archive --format=tar HEAD -o openEMS-$GITREV.tar
+git archive --format=tar --prefix=openEMS/ HEAD -o openEMS-$GITREV.tar
 
 for mod in fparser CSXCAD QCSXCAD AppCSXCAD openEMS CTB hyp2mat; do
   cd $mod || exit 1
   tmpfn=`mktemp --suffix=tar`
-  git archive --format=tar --prefix=$mod/ HEAD -o $tmpfn
+  git archive --format=tar --prefix=openEMS/$mod/ HEAD -o $tmpfn
   cd ..
   tar --concatenate --file=openEMS-$GITREV.tar $tmpfn
   rm -f $tmpfn
