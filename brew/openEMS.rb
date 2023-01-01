8e3efb417be249e1e5b88d4be4db5c221b094231
aWZHY0yQH81uOYvH <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2023-01-01 04:28:46 -0800

    update brew dependencies

--- brew/openEMS.rb
+++ brew/openEMS.rb
@@ -1,6 +1,7 @@
 require "formula"
 
 class Openems < Formula
+  desc "Electromagnetic field solver using the FDTD method"
   homepage "http://openems.de"
 
   head do
@@ -8,17 +9,18 @@ class Openems < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "flex"
-  depends_on "bison"
-  depends_on "qt"
+  depends_on "qt@5"
+  depends_on "vtk@8.2" => ["with-qt", "c++11"]
   depends_on "tinyxml"
-  depends_on "vtk" => ["with-qt", "c++11"]
+  depends_on "hdf5"
+  depends_on "gmp"
+  depends_on "mpfr"
   depends_on "cgal" => "c++11"
   depends_on "boost" => "c++11"
 
   def install
     system "cmake", ".", *std_cmake_args
-    system "make" 
+    system "make"
     # install is handled by ExternalProject_Add
   end
 
