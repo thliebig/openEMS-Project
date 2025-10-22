39f386ed6d835532c3dc3904e1cea153f145df70
Yifeng Li <tomli@tomli.me>
2025-10-22 00:37:44 +0000

    build_python.sh: add local system libraries to search paths
    In additional to libraries in "$INSTALL_PATH", we also need to list
    custom headers and libraries installed to the local system which are
    not used by default. For example, if a custom Boost is installed on
    CentOS, the paths "-L /usr/local/include" and "-R /usr/local/lib" must
    be listed.
    
    On macOS, all Homebrew packages require prefixes
    "-L $(brew --prefix)/include" and "-R $(brew --prefix)/lib" respectively.
    
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -18,11 +18,30 @@ PY_INST_USER=''
 if [[ $EUID != 0 ]] && [[ $PY_INST_IS_VENV != 1 ]]; then
     PY_INST_USER='--user'
 fi
+
+# In additional to libraries in $INSTALL_PATH, we also need to list
+# custom headers and libraries installed to the local system which are
+# not used by default. For example, if a custom Boost is installed on
+# CentOS, the paths -L /usr/local/include and -R /usr/local/lib must
+# be listed.
+SYSLOCAL="/usr/local"
+
+# On macOS, all Homebrew packages belong to this category, and the
+# required prefix are -L $(brew --prefix)/include and
+# -R $(brew --prefix)/lib respectively.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SYSLOCAL="$(brew --prefix)"
+fi
+
 for PY_EXT in 'CSXCAD' 'openEMS'
 do
     echo "build $PY_EXT python module ... please wait"
     cd $PY_EXT/python
-    python3 setup.py build_ext -I $INSTALL_PATH/include -L $INSTALL_PATH/lib -R $INSTALL_PATH/lib && python3 setup.py install $PY_INST_USER
+    python3 setup.py build_ext \
+        -I "$INSTALL_PATH/include:$SYSLOCAL/include" \
+        -L "$INSTALL_PATH/lib:$SYSLOCAL/lib" \
+        -R $INSTALL_PATH/lib
+    python3 setup.py install $PY_INST_USER
     EC=$?
     if [ $EC -ne 0 ]; then
         echo "Python module build failed!"
